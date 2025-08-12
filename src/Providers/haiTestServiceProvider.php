54a5429816ca996fdd7b6aa0ad987ae20809702e
Hai Huynh <hai.huynh@hometogo.com>
2025-08-12 15:05:37 +0700

    tried

--- src/Providers/haiTestServiceProvider.php
+++ src/Providers/haiTestServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace haiTest\Providers;
 
+use IO\Helper\TemplateContainer;
+use IO\Helper\ResourceContainer;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
+
 
 /**
  * Class haiTestServiceProvider
@@ -20,14 +23,26 @@ class haiTestServiceProvider extends ServiceProvider
         $this->getApplication()->register(haiTestRouteServiceProvider::class);
     }
 
+//    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+//    {
+//        // This event listener adds the content from our Twig template
+//        // to the 'IO.tpl.body.end' container. This container is rendered
+//        // just before the closing </body> tag in the storefront.
+////        $eventDispatcher->listen('IO.tpl.body.end', function (Twig $twig) {
+//        $eventDispatcher->listen('IO.Resources.Import', function (Twig $twig) {
+//            return $twig->render('haiTest::bodyEndScript');
+//        });
+//    }
+
+    /**
+     * Boot a template for the basket that will be displayed in the template plugin instead of the original basket.
+     */
     public function boot(Twig $twig, Dispatcher $eventDispatcher)
     {
-        // This event listener adds the content from our Twig template
-        // to the 'IO.tpl.body.end' container. This container is rendered
-        // just before the closing </body> tag in the storefront.
-//        $eventDispatcher->listen('IO.tpl.body.end', function (Twig $twig) {
-        $eventDispatcher->listen('IO.Resources.Import', function (Twig $twig) {
-            return $twig->render('haiTest::bodyEndScript');
-        });
+        $eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container)
+        {
+            // The script is imported in the Footer.twig of plentyShop LTS
+            $container->addScriptTemplate('haiTest::bodyEndScript');
+        }, 10);
     }
 }
